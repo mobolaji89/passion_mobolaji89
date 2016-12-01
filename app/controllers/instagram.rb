8586751ff511db8193455dfc4b2e944f40bcdbfd
Mobolaji David <davidmo@colorado.edu>
2016-12-01 11:46:22 -0800

    modify methods in instagram.rb, and fix issues with .env

--- app/controllers/instagram.rb
+++ app/controllers/instagram.rb
@@ -3,8 +3,8 @@ enable :sessions
 CALLBACK_URL = "http://localhost:9393/auth/callback"
 
 Instagram.configure do |config|
-  p config.client_id = "8f78b349a86a4d79908869c6c54d3c36"
-  p config.client_secret = "03c08b00439649c88255ffc31fb45349"
+  p config.client_id = ENV['CLIENT_ID']
+  p config.client_secret = ENV['CLIENT_SECRET']
   # For secured endpoints only
   #config.client_ips = '<Comma separated list of IPs>'
 end
@@ -24,12 +24,7 @@ get "/auth/callback" do
 end
 
 get "/nav" do
-  html =
-  """
-    <h1>Ruby Instagram Gem Sample Application</h1>
-    <a href='/user_recent_media'>User Recent Media</a> Calls user_recent_media - Get a list of a user's most recent media
-  """
-  html
+  erb :instagram
 end
 
 get "/user_recent_media" do
@@ -37,19 +32,7 @@ get "/user_recent_media" do
   user = client.user
   html = "<h1>#{user.username}'s recent media</h1>"
   for media_item in client.user_recent_media
-    html << "<div style='float:left;'><img src='#{media_item.images.thumbnail.url}'><br/> <a href='/media_like/#{media_item.id}'>Like</a>  <a href='/media_unlike/#{media_item.id}'>Un-Like</a>  <br/>LikesCount=#{media_item.likes[:count]}</div>"
+    html << "<div style='float:left;'><img src='#{media_item.images.thumbnail.url}'><br/> <br/>Likes: #{media_item.likes[:count]}</div>"
   end
   html
-end
-
-get '/media_like/:id' do
-  client = Instagram.client(:access_token => session[:access_token])
-  client.like_media("#{params[:id]}")
-  redirect "/user_recent_media"
-end
-
-get '/media_unlike/:id' do
-  client = Instagram.client(:access_token => session[:access_token])
-  client.unlike_media("#{params[:id]}")
-  redirect "/user_recent_media"
 end
