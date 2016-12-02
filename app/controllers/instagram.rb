9f8dc251b7508a4d7f3eb211e3331688898ab3a4
Mobolaji David <davidmo@colorado.edu>
2016-12-01 17:12:53 -0800

    adjust registration form html and css, bootstrap

--- app/controllers/instagram.rb
+++ app/controllers/instagram.rb
@@ -9,7 +9,7 @@ Instagram.configure do |config|
   #config.client_ips = '<Comma separated list of IPs>'
 end
 
-get "/" do
+get "/instagram" do
   '<a href="/auth/connect">Connect with Instagram</a>'
 end
 
@@ -20,11 +20,11 @@ end
 get "/auth/callback" do
   response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
   session[:access_token] = response.access_token
-  redirect "/nav"
+  redirect "/feed"
 end
 
-get "/nav" do
-  erb :instagram
+get "/feed" do
+  erb :'/feed/instagram'
 end
 
 get "/user_recent_media" do
